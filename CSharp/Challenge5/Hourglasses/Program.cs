fc1c1e042a5eafb9c5ce16d60f23afe26e4f560a
jnbeach <joseph.n.beach@outlook.com>
2021-11-10 17:52:07 -0600

    Challenge 5
    Completed Hourglass and Ice Cream Cones

---
+++ CSharp/Challenge5/Hourglasses/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Hourglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Hourglasses");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Please give me an integer greater than 0, and I will print out an Hourglass of that size.");
+            int maxSize = Convert.ToInt32(Console.ReadLine());
+            int lineCounter = 0;
+            for (int i = maxSize; i > 0; i -= 2)
+            {
+                Console.Write("\n");
+                for (int k = 0; k < lineCounter; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                lineCounter++;
+            }
+
+            for (int i = maxSize; i > 0; i -= 2)
+            {
+                Console.Write("\n");
+                for (int k = 0; k < lineCounter - 1; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = maxSize % 2; j < maxSize - i + 2; j++)
+                {
+                    Console.Write("*");
+                }
+                lineCounter--;
+            }
+
+
+
+
+
+
+
+
+            // -----This still only works for odd numbers -------------------
+            // for (int i = 0; i < maxSize; i += 2)
+            // {
+            //     Console.Write("\n");
+            //     for (int k = newLineCounter - 1; k > 0; k--)
+            //     {
+            //         Console.Write(" ");
+            //     }
+            //     for (int j = 0; j < i + 1; j++)
+            //     {
+            //         Console.Write("*");
+            //     }
+            //     newLineCounter--;
+            // }
+        }
+    }
+}
---
+++ CSharp/Challenge5/IceCreamCones/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IceCreamCones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Ice Cream Cones");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Please give me an integer greater than 0, and I will print out an ice cream cone of that size.");
+            int maxSize = int.Parse(Console.ReadLine());
+            int lineCounter = 0;
+            for (int i = maxSize; i > 0; i -= 2)
+            {
+                Console.Write("\n");
+                for (int k = 0; k < lineCounter; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                lineCounter++;
+            }
+        }
+    }
+}
